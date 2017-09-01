b2d5d981830c1324a51584fe73e32da9e729d812
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2017-08-31 21:51:17 -0700

    Add port tester

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket('ws://www.host.com/path');
+const wss = new WebSocket.Server({ port: 7080 });
 
-ws.on('open', function open() {
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
   ws.send('something');
-});
-
-ws.on('message', function incoming(data) {
-  console.log(data);
 });
